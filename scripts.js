2888079bef3c807a99c3ac487bb13e030b26a4be
MatthewHamilton2 <99368699+MatthewHamilton2@users.noreply.github.com>
2022-02-10 09:53:38 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+
+function showhide(id1, id2){
+	document.getElementById(id1).style.display = "none";
+	document.getElementById(id2).style.display = "block";
+}
+
+function change(id1, id2){
+	document.getElementById(id1).src=(id2)
+}
